d49e8bafcaacdc03d07c3ba33e2ecc922e7bcd4e
JayKarhade <jaykarhade@yahoo.co.in>
2024-04-15 14:03:13 -0400

    first version of adding place rec evaluation

---
+++ place_rec_evaluate/custom_datasets/thermal_dataloader.py
@@ -0,0 +1,215 @@
+import os
+import sys
+from pathlib import Path
+# Set the './../' from the script folder
+dir_name = None
+try:
+    dir_name = os.path.dirname(os.path.realpath(__file__))
+except NameError:
+    print('WARN: __file__ not found, trying local')
+    dir_name = os.path.abspath('')
+lib_path = os.path.realpath(f'{Path(dir_name).parent}')
+# Add to path
+if lib_path not in sys.path:
+    print(f'Adding library path: {lib_path} to PYTHONPATH')
+    sys.path.append(lib_path)
+else:
+    print(f'Library path {lib_path} already in PYTHONPATH')
+
+import os
+import numpy as np
+import cv2
+import torch
+import torch.utils.data 
+from typing import List, Union
+from natsort import natsorted
+from configs import prog_args
+from scipy.spatial.transform import Rotation
+from scipy.spatial.distance import euclidean
+
+from torch.utils.data import DataLoader
+
+import os
+import torch
+import faiss
+import numpy as np
+from PIL import Image
+import torchvision.transforms as T
+from sklearn.neighbors import NearestNeighbors
+
+from utilities import CustomDataset
+from custom_datasets.ms2_utils import *
+
+def path_to_pil_img(path):
+    return Image.open(path).convert("RGB")
+
+base_transform = T.Compose([
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+mixVPR_transform = T.Compose([
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225]),
+    T.Resize((320,320))
+])
+
+
+class Thermal_day_night_MS2(CustomDataset):
+    """
+    Returns dataset class with images from database and queries for the vpair dataset. 
+    """
+    def __init__(self,args,datasets_folder='/ocean/projects/cis220039p/shared/datasets/MS2_full',dataset_name="sync_data",split="train",use_ang_positives=False,dist_thresh = 10,ang_thresh=20,use_mixVPR=False,use_SAM=False):
+        super().__init__()
+
+        self.dataset_name = dataset_name
+        self.datasets_folder = datasets_folder
+        self.split = split
+        self.use_mixVPR = use_mixVPR
+        self.use_SAM = use_SAM
+        # self.seq = "_2021-08-06-10-59-33"
+        self.seq = "_2021-08-06-11-23-45"
+        # self.seq = "_2021-08-06-11-23-45"
+        print("seq: ",self.seq)
+        # self.db_paths = natsorted(os.listdir(os.path.join(self.datasets_folder,self.dataset_name,self.seq,"rgb/img_left")))[::20]
+        self.db_paths = natsorted(os.listdir(os.path.join(self.datasets_folder,self.dataset_name,self.seq,"thr/img_left")))[::20]
+        # self.db_paths = natsorted(os.listdir(os.path.join(self.datasets_folder,"proj_depth/",self.seq,"rgb", "depth_filtered")))[::20]
+        # self.q_paths = natsorted(os.listdir(os.path.join(self.datasets_folder,self.dataset_name,self.seq,"rgb/img_left")))[::20]
+        # self.q_paths = natsorted(os.listdir(os.path.join(self.datasets_folder,self.dataset_name,self.seq,"thr/img_left")))[::20]
+        self.q_paths = natsorted(os.listdir(os.path.join(self.datasets_folder,"proj_depth/",self.seq,"rgb", "depth_filtered")))[::20]
+        self.db_abs_paths = []
+        self.q_abs_paths = []
+
+        for p in self.db_paths:
+            self.db_abs_paths.append(os.path.join(self.datasets_folder,self.dataset_name,self.seq,"thr/img_left",p))
+            # self.db_abs_paths.append(os.path.join(self.datasets_folder,"proj_depth/",self.seq,"rgb", "depth_filtered",p))
+            # self.db_abs_paths.append(os.path.join(self.datasets_folder,self.dataset_name,self.seq,"rgb/img_left",p))
+        for q in self.q_paths:
+            # self.q_abs_paths.append(os.path.join(self.datasets_folder,self.dataset_name,self.seq,"rgb/img_left",q))
+            self.q_abs_paths.append(os.path.join(self.datasets_folder,"proj_depth/",self.seq,"rgb", "depth_filtered",q))
+            # self.q_abs_paths.append(os.path.join(self.datasets_folder,self.dataset_name,self.seq,"thr/img_left",q))
+        self.db_num = len(self.db_abs_paths)
+        self.q_num = len(self.q_abs_paths)
+
+        self.database_num = self.db_num
+        self.queries_num = self.q_num
+
+        self.form_gt_positives()
+
+        self.images_paths = list(self.db_abs_paths) + list(self.q_abs_paths)
+
+    def form_gt_positives(self):
+        """
+        Returns ground truth positives for the dataset.
+        """
+
+        # load files for the coordinates
+        self.db_coord_paths = natsorted(os.listdir(os.path.join(self.datasets_folder,"odom",self.seq,"thr")))[::20]
+        self.q_coord_paths = natsorted(os.listdir(os.path.join(self.datasets_folder,"odom",self.seq,"thr")))[::20]
+        self.db_coords = []
+        self.q_coords = []
+
+        for p in self.db_coord_paths:          
+            with open(os.path.join(self.datasets_folder,"odom",self.seq,"thr",p)) as f:
+                lines = f.readline()
+                elements = lines.split()
+
+                matrix = []
+                for i in range(0, len(elements), 4):
+                    row = [float(elements[i]), float(elements[i+1]), float(elements[i+2]), float(elements[i+3])]
+                    matrix.append(row)
+                matrix = np.asarray(matrix)
+
+                coord_x = float(matrix[0,3])
+                coord_y = float(matrix[1,3])
+                coord_z = float(matrix[2,3])
+
+                self.db_coords.append([coord_x,coord_y,coord_z])
+
+        for q in self.q_coord_paths:
+            with open(os.path.join(self.datasets_folder,"odom",self.seq,"thr",q)) as f:
+                lines = f.readline()
+                elements = lines.split()
+
+                matrix = []
+                for i in range(0, len(elements), 4):
+                    row = [float(elements[i]), float(elements[i+1]), float(elements[i+2]), float(elements[i+3])]
+                    matrix.append(row)
+                matrix = np.asarray(matrix)
+
+                coord_x = float(matrix[0,3])
+                coord_y = float(matrix[1,3])
+                coord_z = float(matrix[2,3])
+
+                self.q_coords.append([coord_x,coord_y,coord_z])
+
+        # do knn over the coordinates
+        knn = NearestNeighbors(n_jobs=-1)
+        knn.fit(self.db_coords)
+        self.dist,self.soft_positives_per_query = knn.radius_neighbors(self.q_coords,
+                                                            radius= 25,
+                                                            return_distance=True)            
+
+        # for i in range(len(self.soft_positives_per_query)):
+        #     print("query: ",self.soft_positives_per_query[i],self.q_paths[i])
+            # for j in range(len(self.soft_positives_per_query[i])):
+            #     print("database: ",self.db_paths[self.soft_positives_per_query[i][j]],self.dist[i][j])
+
+    def __getitem__(self, index):
+
+        if index>=self.database_num:
+            # # img = cv2.imread(self.images_paths[index])
+            # # img = base_transform(img)
+            # img = cv2.imread(self.images_paths[index])
+            # h  = img.shape[0]
+            # w = img.shape[1]
+            # img = cv2.resize(img, ((w//14)*14, (h//14)*14))
+            # img = base_transform(img)
+
+            # thermal_image_path1 = self.images_paths[index]
+            # img = load_as_float_img(thermal_image_path1)
+            # img = process_one_image(img,type="hist_99")
+            # img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+            # h = img.shape[0]
+            # w = img.shape[1]
+            # img = cv2.resize(img, ((w//14)*14, (h//14)*14))
+            # img = base_transform(img)
+
+            lidar_image_path1 = self.images_paths[index]
+            img = cv2.imread(lidar_image_path1)
+            h = img.shape[0]
+            w = img.shape[1]
+            img = cv2.resize(img, ((w//14)*14, (h//14)*14))
+            img = base_transform(img)
+
+        elif index<self.database_num:
+            thermal_image_path1 = self.images_paths[index]
+            img = load_as_float_img(thermal_image_path1)
+            img = process_one_image(img,type="hist_99")
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+            h = img.shape[0]
+            w = img.shape[1]
+            img = cv2.resize(img, ((w//14)*14, (h//14)*14))
+            img = base_transform(img)
+
+            # img = cv2.imread(self.images_paths[index])
+            # h  = img.shape[0]
+            # w = img.shape[1]
+            # img = cv2.resize(img, ((w//14)*14, (h//14)*14))
+            # img = base_transform(img)
+
+            # lidar_image_path1 = self.images_paths[index]
+            # img = cv2.imread(lidar_image_path1)
+            # h = img.shape[0]
+            # w = img.shape[1]
+            # img = cv2.resize(img, ((w//14)*14, (h//14)*14))
+            # img = base_transform(img)
+
+        return img, index
+
+if __name__ == "__main__":
+    args = None
+    dataset = Thermal_day_night_MS2(args,split="train",use_mixVPR=False)
+    print(dataset[0][0].shape)
+    
---
+++ place_rec_evaluate/dino_v2_plot_qual.py
@@ -0,0 +1,413 @@
+# Doing VLAD with Dino V2 descriptors
+"""
+    Basic idea is to explore the layers and facets of Dino-v2 and do
+    VLAD over them.
+"""
+
+import os
+import sys
+
+from dataclasses import dataclass, field
+import einops as ein
+import faiss
+import faiss.contrib.torch_utils
+import matplotlib.pyplot as plt
+import joblib
+import numpy as np
+from PIL import Image
+import torch
+from torch.nn import functional as F
+from torchvision import transforms as T
+from torch.utils.data import DataLoader
+
+import time
+import traceback
+from tqdm.auto import tqdm
+import tyro
+from typing import Union, Literal, Tuple, List
+
+import wandb
+
+from utilities import * 
+from configs import ProgArgs, prog_args, BaseDatasetArgs, \
+        base_dataset_args, device
+from custom_datasets.thermal_dataloader import Thermal_day_night_MS2
+
+@dataclass
+class LocalArgs:
+    # Program arguments (dataset directories and wandb)
+    prog: ProgArgs = ProgArgs(wandb_proj="Dino-v2-Descs", 
+        wandb_group="VLAD-Descs")
+    # BaseDataset arguments
+    bd_args: BaseDatasetArgs = base_dataset_args
+    # Experiment identifier (None = don't use)
+    exp_id: Union[str, None] = None
+    # Dino parameters
+    # Model type
+    model_type: Literal["dinov2_vits14", "dinov2_vitb14", 
+            "dinov2_vitl14", "dinov2_vitg14","dinov2_vits14_reg"] = "dinov2_vits14"
+    """
+        Model for Dino-v2 to use as the base model.
+    """
+    # Number of clusters for VLAD
+    num_clusters: int = 8
+    # Layer for extracting Dino feature (descriptors)
+    desc_layer: int = 11
+    # Facet for extracting descriptors
+    desc_facet: Literal["query", "key", "value", "token"] = "key"
+    # Dataset split for VPR (BaseDataset)
+    data_split: Literal["train", "test", "val"] = "test"
+    # Sub-sample query images (RAM or VRAM constraints) (1 = off)
+    sub_sample_qu: int = 1
+    # Sub-sample database images (RAM or VRAM constraints) (1 = off)
+    sub_sample_db: int = 1
+    # Sub-sample database images for VLAD clustering only
+    sub_sample_db_vlad: int = 1
+    """
+        Use sub-sampling for creating the VLAD cluster centers. Use
+        this to reduce the RAM usage during the clustering process.
+        Unlike `sub_sample_qu` and `sub_sample_db`, this is only used
+        for clustering and not for the actual VLAD computation.
+    """
+    # Values for top-k (for monitoring)
+    top_k_vals: List[int] = field(default_factory=lambda:\
+                                list(range(1, 21, 1)))
+    # Show a matplotlib plot for recalls
+    show_plot: bool = False
+    # Use hard or soft descriptor assignment for VLAD
+    vlad_assignment: Literal["hard", "soft"] = "hard"
+    # Softmax temperature for VLAD (soft assignment only)
+    vlad_soft_temp: float = 1.0
+    # Caching configuration
+    cache_vlad_descs: bool = False
+
+    #retreival stuff
+    batch_size: int = 1
+    qual_num_rets: int = 5
+    faiss_method: Literal["l2", "cosine"] = "cosine"
+    use_residual: Literal[1, 0] = bool(0)
+    qual_result_percent: float = 0.1
+
+def plot_recalls(largs: LocalArgs, ndb_descs: np.ndarray, 
+            nqu_descs: np.ndarray, pos_per_qu: np.ndarray,
+            vpr_dl:Union[None, DataLoader]=None, use_percentage=True, 
+            use_gpu: bool=True, save_figs:bool= True):
+    """
+        Calculate the recalls through similarity search (using cosine
+        distances).
+        
+        Parameters:
+        - largs:    Local arguments to program. The following are used
+                    - top_k_vals: For getting keys
+        - ndb_descs:    Normalized database descriptors [N_d, D]
+        - nqu_descs:    Normalized query descriptors [N_q, D]
+        - pos_per_qu:   Positives (within a distance threshold) per
+                        query index. [N_qu, ] list (object) with each
+                        index containing positive sample indices.
+        - vpr_dl:       DataLoader for images (used for getting 
+                        qualitative results). Pass None if certain of
+                        no qualitative results (see `save_figs`).
+        - use_percentage:   If true, the recall is between [0, 1] and
+                            not absolute. It's divided by N_q.
+        - use_gpu:      Use GPU for faiss (else use CPU)
+        - save_figs:    Save the qualitative results (if False, no
+                        qualitative results are saved, and if True,
+                        then saving depends on LocalArgs `exp_id`)
+        
+        Returns:
+        - recalls: A dictionary of retrievals
+    """
+    # Saving preferences
+    query_color = (125,   0, 125)   # RGB for query image (1st)
+    false_color = (255,   0,   0)   # False retrievals
+    true_color =  (  0, 255,   0)   # True retrievals
+    padding = 20
+    qimgs_result, qimgs_dir = True, \
+        f"{largs.prog.cache_dir}/qualitative_retr" # Directory
+    if largs.exp_id == False or largs.exp_id is None:   # Don't store
+        qimgs_result, qimgs_dir = False, None
+    elif type(largs.exp_id) == str:
+        if not largs.use_residual:
+            qimgs_dir = f"{largs.prog.cache_dir}/experiments/"\
+                        f"{largs.exp_id}/qualitative_retr"
+        else:
+            qimgs_dir = f"{largs.prog.cache_dir}/experiments/"\
+                        f"{largs.exp_id}/qualitative_retr_residual_nc"\
+                        f"{largs.num_clusters}"
+    qimgs_inds = []
+    if (not save_figs) or largs.qual_result_percent <= 0:
+        qimgs_result = False
+    if not qimgs_result:    # Saving query images
+        print("Not saving qualitative results")
+    else:
+        _n_qu = nqu_descs.shape[0]
+        qimgs_inds = np.random.default_rng().choice(
+                range(_n_qu), int(_n_qu * largs.qual_result_percent),
+                replace=False)  # Qualitative images to save
+        print(f"There are {_n_qu} query images")
+        print(f"Will save {len(qimgs_inds)} qualitative images")
+        if not os.path.isdir(qimgs_dir):
+            os.makedirs(qimgs_dir)  # Ensure folder exists
+            print(f"Created qualitative directory: {qimgs_dir}")
+        else:
+            print(f"Saving qualitative results in: {qimgs_dir}")
+    # FAISS search
+    max_k = max(largs.top_k_vals)
+    D = ndb_descs.shape[1]
+    recalls = dict(zip(largs.top_k_vals, [0]*len(largs.top_k_vals)))
+    if largs.faiss_method == "cosine":
+        index = faiss.IndexFlatIP(D)
+    elif largs.faiss_method == "l2":
+        index = faiss.IndexFlatL2(D)
+    else:
+        raise Exception(f"FAISS method: {largs.faiss_method}!")
+    if use_gpu:
+        print("Running GPU faiss index")
+        res = faiss.StandardGpuResources()  # use a single GPU
+        index = faiss.index_cpu_to_gpu(res, 0, index)
+    index.add(ndb_descs)    # Add database
+    # Distances and indices are [N_q, max_k] shape
+    distances, indices = index.search(nqu_descs, max_k) # Query
+    for i_qu, qu_retr_maxk in enumerate(indices):
+        for i_rec in largs.top_k_vals:
+            correct_retr_qu = pos_per_qu[i_qu]  # Ground truth
+            if np.any(np.isin(qu_retr_maxk[:i_rec], correct_retr_qu)):
+                recalls[i_rec] += 1 # Query retrieved correctly
+        if i_qu in qimgs_inds and qimgs_result:
+            # Save qualitative results
+            qual_top_k = qu_retr_maxk[:largs.qual_num_rets]
+            correct_retr_qu = pos_per_qu[i_qu]
+            color_mask = np.isin(qual_top_k, correct_retr_qu)
+            print(qual_top_k,correct_retr_qu)
+            colors_all = [true_color if x else false_color \
+                        for x in color_mask]
+            retr_dists = distances[i_qu, :largs.qual_num_rets]
+            img_q = to_pil_list(    # Dataset is [database] + [query]
+                vpr_dl.dataset[ndb_descs.shape[0]+i_qu][0])[0]
+            img_q = to_np(img_q, np.uint8)
+            # Main figure
+            fig = plt.figure(figsize=(5*(1+largs.qual_num_rets), 5),
+                            dpi=300)
+            gs = fig.add_gridspec(1, 1+largs.qual_num_rets)
+            ax = fig.add_subplot(gs[0, 0])
+            ax.set_title(f"{i_qu} + {ndb_descs.shape[0]}")  # DS index
+            ax.imshow(pad_img(img_q, padding, query_color))
+            ax.axis('off')
+            for i, db_retr in enumerate(qual_top_k):
+                ax = fig.add_subplot(gs[0, i+1])
+                img_r = to_pil_list(vpr_dl.dataset[db_retr][0])[0]
+                img_r = to_np(img_r, np.uint8)
+                ax.set_title(f"{db_retr} ({retr_dists[i]:.4f})")
+                ax.imshow(pad_img(img_r, padding, colors_all[i]))
+                ax.axis('off')
+            fig.set_tight_layout(True)
+            save_path = f"{qimgs_dir}/Q_{i_qu}_Top_"\
+                        f"{largs.qual_num_rets}.png"
+            fig.savefig(save_path)
+            plt.close(fig)
+            if largs.prog.use_wandb and largs.prog.wandb_save_qual:
+                wandb.log({"Qual_Results": wandb.Image(save_path)})
+    if use_percentage:
+        for k in recalls:
+            recalls[k] /= len(indices)  # As a percentage of queries
+    return recalls
+
+def build_descriptors(largs: LocalArgs, vpr_ds, verbose: bool=True)-> Tuple[torch.Tensor, torch.Tensor]:
+
+    # Load Dino feature extractor model
+    # dino_db = DinoV2ExtractFeatures(largs.model_type, largs.desc_layer,
+    #             largs.desc_facet,modality='rgb', device=device)
+    # dino_q = DinoV2ExtractFeatures(largs.model_type, largs.desc_layer,
+    #             largs.desc_facet,modality='thermal', device=device)
+
+    dino_db = torch.hub.load('facebookresearch/dinov2', largs.model_type).cuda()
+    dino_q = torch.hub.load('facebookresearch/dinov2', largs.model_type).cuda()
+    dino_db.load_state_dict(torch.load("/ocean/projects/cis220039p/jkarhade/MultiLoc/pretraining/checkpoints/checkpoints_thermal_global/thermal9.pth")["model_state_dict"])
+    dino_q.load_state_dict(torch.load("/ocean/projects/cis220039p/jkarhade/MultiLoc/pretraining/checkpoints/checkpoints_lidar_global/lidar9.pth")["model_state_dict"])
+
+    if verbose:
+        print("Dino model loaded")
+
+    def extract_patch_descriptors_db(indices,allow_flip=False):
+        print("allow flipping",allow_flip)
+        patch_descs = []
+        for i in tqdm(indices, disable=not verbose):
+            img = vpr_ds[i][0].to(device)
+            c, h, w = img.shape
+            h_new, w_new = (h // 14) * 14, (w // 14) * 14
+            img_in = T.CenterCrop((h_new, w_new))(img)[None, ...]
+            if allow_flip:
+                flipper = T.RandomHorizontalFlip(p=1)
+                img_in = flipper(img_in)
+            ret = dino_db(img_in)
+            patch_descs.append(ret.cpu())
+        patch_descs = torch.cat(patch_descs, dim=0) # [N, n_p, d_dim]
+
+        return patch_descs
+
+    def extract_patch_descriptors_q(indices,allow_flip=False):
+        print("allow flipping",allow_flip)
+        patch_descs = []
+        for i in tqdm(indices, disable=not verbose):
+            img = vpr_ds[i][0].to(device)
+            c, h, w = img.shape
+            h_new, w_new = (h // 14) * 14, (w // 14) * 14
+            img_in = T.CenterCrop((h_new, w_new))(img)[None, ...]
+            if allow_flip:
+                flipper = T.RandomHorizontalFlip(p=1)
+                img_in = flipper(img_in)
+            ret = dino_q(img_in)
+            patch_descs.append(ret.cpu())
+        patch_descs = torch.cat(patch_descs, dim=0) # [N, n_p, d_dim]
+
+        return patch_descs
+
+    # Get the database descriptors
+    num_db = vpr_ds.database_num
+    ds_len = len(vpr_ds)
+    assert ds_len > num_db, "Either no queries or length mismatch"
+    
+    # Get descriptors of the database
+    if verbose:
+        print("Building VLADs for databases...")
+    db_indices = np.arange(0, num_db, largs.sub_sample_db)
+    db_img_names = vpr_ds.get_image_relpaths(db_indices)
+
+    full_db = extract_patch_descriptors_db(db_indices,allow_flip=False)
+    if verbose:
+        print(f"Full database descriptor shape: {full_db.shape}")
+    
+    # Get descriptors of the queries
+    if verbose:
+        print("Building VLADs for queries...")
+    qu_indices = np.arange(num_db, ds_len, largs.sub_sample_qu)
+    qu_img_names = vpr_ds.get_image_relpaths(qu_indices)
+
+    full_qu = []
+    full_qu = extract_patch_descriptors_q(qu_indices)
+    if verbose:
+        print(f"Full query descriptor shape: {full_qu.shape}")
+    
+    return full_db, full_qu
+
+@torch.no_grad()
+def main(largs: LocalArgs):
+    print(f"Arguments: {largs}")
+    seed_everything(42)
+
+    if largs.prog.use_wandb:
+        # Launch WandB
+        wandb_run = wandb.init(project=largs.prog.wandb_proj, 
+                entity=largs.prog.wandb_entity, config=largs,
+                group=largs.prog.wandb_group, 
+                name=largs.prog.wandb_run_name)
+        print(f"Initialized WandB run: {wandb_run.name}")
+    
+    print("--------- Generating VLADs ---------")
+    ds_dir = largs.prog.data_vg_dir
+    ds_name = largs.prog.vg_dataset_name
+    print(f"Dataset directory: {ds_dir}")
+    print(f"Dataset name: {ds_name}, split: {largs.data_split}")
+
+    # Load dataset
+    if ds_name=="thermal_day_night":
+        vpr_ds = Thermal_day_night_MS2(largs.bd_args)
+    
+    db_vlads, qu_vlads = build_descriptors(largs, vpr_ds)
+    print("--------- Generated VLADs ---------")
+    
+    print("----- Calculating recalls through top-k matching -----")
+    dists, indices, recalls = get_top_k_recall(largs.top_k_vals, 
+        db_vlads, qu_vlads, vpr_ds.soft_positives_per_query, 
+        sub_sample_db=largs.sub_sample_db, 
+        sub_sample_qu=largs.sub_sample_qu)
+    print("------------ Recalls calculated ------------")
+
+    # Plot recalls
+    print("------------ Plot Recalls ------------")
+    vpr_dl = DataLoader(vpr_ds, largs.batch_size, pin_memory=True, 
+                        shuffle=False)
+
+    print("--------------------- Results ---------------------")
+    ts = time.strftime(f"%Y_%m_%d_%H_%M_%S")
+    caching_directory = largs.prog.cache_dir
+    results = {
+        "Model-Type": str(largs.model_type),
+        "Desc-Layer": str(largs.desc_layer),
+        "Desc-Facet": str(largs.desc_facet),
+        "Desc-Dim": str(db_vlads.shape[1]//largs.num_clusters),
+        "VLAD-Dim": str(db_vlads.shape[1]),
+        "Num-Clusters": str(largs.num_clusters),
+        "Experiment-ID": str(largs.exp_id),
+        "DB-Name": str(ds_name),
+        "Num-DB": str(len(db_vlads)),
+        "Num-QU": str(len(qu_vlads)),
+        "Agg-Method": "VLAD",
+        "Timestamp": str(ts)
+    }
+    print("Results: ")
+    for k in results:
+        print(f"- {k}: {results[k]}")
+    print("- Recalls: ")
+    for k in recalls:
+        results[f"R@{k}"] = recalls[k]
+        print(f"  - R@{k}: {recalls[k]:.5f}")
+    if largs.show_plot:
+        plt.plot(recalls.keys(), recalls.values())
+        plt.ylim(0, 1)
+        plt.xticks(largs.top_k_vals)
+        plt.xlabel("top-k values")
+        plt.ylabel(r"% recall")
+        plt_title = "Recall curve"
+        if largs.exp_id is not None:
+            plt_title = f"{plt_title} - Exp {largs.exp_id}"
+        if largs.prog.use_wandb:
+            plt_title = f"{plt_title} - {wandb_run.name}"
+        plt.title(plt_title)
+        plt.show()
+
+    print("------------ Plot Recalls Viz Start------------")
+    plot_recalls(largs, db_vlads, qu_vlads, vpr_ds.soft_positives_per_query,vpr_dl)
+    print("------------ Plot Recalls Viz Finished------------")
+
+    # Log to WandB
+    if largs.prog.use_wandb:
+        wandb.log(results)
+        for tk in recalls:
+            wandb.log({"Recall-All": recalls[tk]}, step=int(tk))
+    
+    # Add retrievals
+    results["Qual-Dists"] = dists
+    results["Qual-Indices"] = indices
+    save_res_file = None
+    if largs.exp_id == True:
+        save_res_file = caching_directory
+    elif type(largs.exp_id) == str:
+        save_res_file = f"{caching_directory}/experiments/"\
+                        f"{largs.exp_id}"
+    if save_res_file is not None:
+        if not os.path.isdir(save_res_file):
+            os.makedirs(save_res_file)
+        save_res_file = f"{save_res_file}/results_{ts}.gz"
+        print(f"Saving result in: {save_res_file}")
+        joblib.dump(results, save_res_file)
+    else:
+        print("Not saving results")
+    
+    if largs.prog.use_wandb:
+        wandb.finish()
+    print("--------------------- END ---------------------")
+
+
+if __name__ == "__main__" :
+    largs = tyro.cli(LocalArgs, description=__doc__)
+    _start = time.time()
+    try:
+        main(largs)
+    except:
+        print("Unhandled exception")
+        traceback.print_exc()
+    finally:
+        print(f"Program ended in {time.time()-_start:.3f} seconds")
+        exit(0)
+
